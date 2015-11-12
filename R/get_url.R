111179e4d1c2b2a38e315911b7db456a5963eb09
Matthew Upson <ivyleavedtoadflax@gmail.com>
2015-11-12 08:26:13 +0000

    Added get_url function

---
+++ R/get_url.R
@@ -0,0 +1,94 @@
+#' @title Get URL
+#'   
+#' @description \code{get_url} download requested url with elegant error 
+#'   handling for output to a data.frame. This function is intended to populate 
+#'   a data.frame with the status of the attempted download of each url, hence 
+#'   will return the download statys as a character string with more friendly 
+#'   error messages
+#'   
+#' @param x \code{x} A url as a character string.
+#' @param x \code{dest} A local file destination for the url to be saved to.
+#'   
+#' @return A character string of either \code{"success"} or a simplified error or warning message, or \code{"unhandled error"}/\code{"unhandled warning"}.
+#'   
+#' @examples
+#' 
+#' library(scrapeML)
+#' 
+#' get_url("www.google.com",tempfile())
+#' 
+#' @export
+
+
+get_url <- function(site_url, dest) {
+  
+  ## Deal with any errors in the inputs
+  
+  stopifnot(
+    class(site_url) == "character",
+    length(site_url) == 1,
+    substr(site_url, 1, 7) == "http://",
+    class(dest) == "character",
+    length(dest) == 1
+  )
+  
+  
+  ## Now try the download with error handling
+  
+  tryCatch(
+    {
+      
+      ## Try to download the file
+      
+      download.file(
+        url = site_url,
+        destfile = dest,
+        quiet = TRUE
+      )
+      
+      message(paste("Successfully downloaded", site_url, "to", dest))
+      
+      ## Return success
+      return("success")
+      # write_log()
+      
+    },
+    error = function(cond) {
+      message(paste("Error in get_Url():"))
+      message(cond, "\n")
+      ## Choose a return value in case of error
+      
+      warn <- "Unhandled error"
+      
+      if (grepl("unsupported URL scheme", cond)) {
+        
+        warn <- "invalid URL"
+        
+        message(warn)
+      }      
+      
+      return(warn)
+    },
+    warning = function(cond) {
+      message(paste("Warning in get_Url():"))
+      message(cond, "\n")
+      ## Choose a return value in case of warning
+      
+      warn <- "Unhandled warning"
+      
+      if (grepl("unable to resolve", cond)) {
+        
+        warn <- "internet not available"
+        
+        message(warn)
+        message("Check connection and try again")
+      }
+      
+      
+      return(warn)
+    },
+    finally = {
+      
+    }
+  )
+}
---
+++ tests/testthat/test_get_url.R
@@ -0,0 +1,28 @@
+
+
+test_that(
+  "get_url successfully handles errors",
+  {
+    
+    ## site_url is not properly formed
+    
+expect_error(get_url("http:/www.google.com", tempfile()))
+    
+    ## site_url is a vector
+    
+expect_error(get_url(c("http://www.google.com","http://www.google.com"), tempfile()))
+    
+    ## site_url is not character
+    
+expect_error(get_url(1, tempfile()))
+    
+    ## dest is character
+    
+expect_error(get_url("http://www.google.com", 1))
+
+  ## dest is signular
+
+expect_error(get_url("http://www.google.com", c(tempfile(),tempfile())))
+
+    }
+  )
